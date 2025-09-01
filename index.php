38193fd27716f6c9afc6852a8e82937642ebe1cc
Bianca Feliciano Lima <biancafeliciano@outlook.com>
2025-09-01 00:26:23 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>MedControl - Início</title>
+    <link rel="stylesheet" href="css/home.css">
+    
+</head>
+<body>
+    <h1 class="logo">Med<span>Control</span> <span class="emoji">🩺</span></h1>
+    <div class="button-container">
+        <a href="../medcontrol/public/login_medico.php"><button>LOGIN MÉDICO</button></a>
+        <a href="../medcontrol/public/login_assistente.php"><button>LOGIN ASSISTENTE</button></a>
+        <a href="../medcontrol/public/cadastro.php"><button>NOVO CADASTRO</button></a>
+    </div>
+</body>
+</html>
